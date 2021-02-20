d73c27f082e0b74b6eee2ede4ecea90f3cbec365
Sachin.B.Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-02-20 14:15:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://duckduckgo.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        #NavBar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('⏪', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('⏩', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('🔃', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('🔼', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+    def navigate_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+    
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://duckduckgo.com'))
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('ALPHA BROWSER')
+window = MainWindow()
+app.exec_()
